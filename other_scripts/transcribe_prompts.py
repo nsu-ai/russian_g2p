2124c19c27a58ffdeeaa48c5bb4d1f962f15e0d7
nsu-ai <36190785+nsu-ai@users.noreply.github.com>
2018-02-18 00:47:58 +0700

    Add files via upload

--- other_scripts/transcribe_prompts.py
+++ other_scripts/transcribe_prompts.py
@@ -1,6 +1,7 @@
 from russian_g2p import Grapheme2Phoneme
 import codecs
 import csv
+from os.path import isfile
 
 prompts_file = open('corpus/corpus_FAS_short_only')
 prompts_lines = prompts_file.readlines()
@@ -9,15 +10,17 @@ with codecs.open('corpus/transcribed_voxforge_fragment.csv', mode='wb', encoding
 	wr = csv.writer(f)
 	for prompts_line in prompts_lines:
 		prompts_line_clean = prompts_line.strip().lower()
+		id_name = prompts_line.split()[0].replace('/mfc/', '/wav/')
 		prompts_words = ' '.join(prompts_line_clean.split()[1:])
 		try:
 			transcription = g2p.phrase_to_phonemes(prompts_words)
-			with open('corpus/dict_voxforge_fragment', 'a') as f:
-				for word in prompts_words.split():
-					f.writelines('{}\t{}\n'.format(word.replace('+', ''), ' '.join(g2p.word_to_phonemes(word))))
-			wr.writerow([prompts_line.strip().split()[0],
-				prompts_words.replace('+', ''),
-				' '.join([' '.join(word) for word in transcription])])
+			if isfile('/media/dino/DATA/corpus/voxforge/repository/downloads/Russian/Trunk/Audio/Main/8kHz_16bit/' + id_name + '.wav'):
+				with open('corpus/dict_voxforge_fragment', 'a') as f:
+					for word in prompts_words.split():
+						f.writelines('{}\t{}\n'.format(word.replace('+', ''), ' '.join(g2p.word_to_phonemes(word))))
+				wr.writerow([id_name,
+					prompts_words.replace('+', ''),
+					' '.join([' '.join(word) for word in transcription])])
 		except AssertionError:
 			continue
 
