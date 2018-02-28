f0b059a2761c1323f37cbb0237abbc8621185a1f
nsu-ai <36190785+nsu-ai@users.noreply.github.com>
2018-02-28 15:20:07 +0700

    Update transcribe_prompts.py

--- other_scripts/transcribe_prompts.py
+++ other_scripts/transcribe_prompts.py
@@ -3,10 +3,10 @@ import codecs
 import csv
 from os.path import isfile
 
-prompts_file = open('corpus/corpus_FAS_short_only')
+prompts_file = open('corpus/fixed_corpus_with_punct+acc+sil')
 prompts_lines = prompts_file.readlines()
 g2p = Grapheme2Phoneme()
-with codecs.open('corpus/transcribed_voxforge_fragment.csv', mode='wb', encoding='utf-8') as f:
+with codecs.open('corpus/transcribed_voxforge_fragment_PUNCT.csv', mode='wb', encoding='utf-8') as f:
 	wr = csv.writer(f)
 	for prompts_line in prompts_lines:
 		prompts_line_clean = prompts_line.strip().lower()
@@ -14,13 +14,13 @@ with codecs.open('corpus/transcribed_voxforge_fragment.csv', mode='wb', encoding
 		prompts_words = ' '.join(prompts_line_clean.split()[1:])
 		try:
 			transcription = g2p.phrase_to_phonemes(prompts_words)
-			if isfile('/media/dino/DATA/corpus/voxforge/repository/downloads/Russian/Trunk/Audio/Main/8kHz_16bit/' + id_name + '.wav'):
-				with open('corpus/dict_voxforge_fragment', 'a') as f:
-					for word in prompts_words.split():
+			with open('corpus/dict_voxforge_fragment_PUNCT', 'a') as f:
+				for word in prompts_words.split():
+					if word != 'sil':
 						f.writelines('{}\t{}\n'.format(word.replace('+', ''), ' '.join(g2p.word_to_phonemes(word))))
-				wr.writerow([id_name,
-					prompts_words.replace('+', ''),
-					' '.join([' '.join(word) for word in transcription])])
+			wr.writerow([id_name,
+				prompts_words.replace('+', ''),
+				' '.join(transcription)])
 		except AssertionError:
 			continue
 
