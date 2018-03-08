0034781051da38ba6da96e3832576633425cbf74
project <masha-1998-b@mail.ru>
2018-03-08 23:00:42 +0700

    v.1.0.0

---
+++ russian_g2p/Transcription.py
@@ -0,0 +1,17 @@
+from russian_g2p.Preprocessor import Preprocessor
+from russian_g2p.Accentor import Accentor
+from russian_g2p.Grapheme2Phoneme import Grapheme2Phoneme
+
+class Transcription:
+    def transcribe(self, text: str):
+        words, tags = Preprocessor().preprocessing(text)
+        accented_text = Accentor().do_accents(words, tags)
+        tmp = ' '.join(accented_text[0])
+        tmp = ' ' + tmp
+        phonetic_words = tmp.split(' <sil>')
+        result = []
+        for phonetic_word in phonetic_words:
+            if len(phonetic_word) != 0:
+                phonemes = Grapheme2Phoneme().phrase_to_phonemes(phonetic_word)
+                result.append(phonemes)
+        return result
