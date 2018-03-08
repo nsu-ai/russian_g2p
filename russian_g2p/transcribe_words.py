4c4711ea58bf8d180d46cc7e90bf82f36d54f1bd
project <masha-1998-b@mail.ru>
2018-03-08 22:59:08 +0700

    Sent wordlist to Transcriptor

--- russian_g2p/transcribe_words.py
+++ russian_g2p/transcribe_words.py
@@ -1,17 +1,18 @@
-from Grapheme2Phoneme import Grapheme2Phoneme
-from Accentor import Accentor
+from russian_g2p import Grapheme2Phoneme
+from russian_g2p import Accentor
 import json
 import codecs
 
-words = open('/home/dino/sphinx/wordlist')
+words = open('/home/a117/Документы/Linguistics/russian_g2p/corpus/wordlist')
 words = words.readlines()
-words = ['я']
-acc = Accentor()
+# words = ['я']
+# acc = Accentor()
 g2p = Grapheme2Phoneme()
 new_simple_words = {}
 with open('new', 'w') as f:
     for word in words:
         word = word.strip()
+        '''
         print(word)
         res, added_simple_words = acc.do_accents([word])
         new_simple_words = {**new_simple_words, **added_simple_words}
@@ -22,8 +23,10 @@ with open('new', 'w') as f:
             print('word has one var', res)
             word2transcribe = res[0][0]
         print(word2transcribe)
-        transcriptions = g2p.word_to_phonemes(word2transcribe)
-        print('{} {}\n'.format(word, ' '.join(transcriptions)))
+        '''
+        word2transcribe = word  # Daniil
+        transcriptions = g2p.phrase_to_phonemes(word2transcribe)
+        # print('{} {}\n'.format(word, ' '.join(transcriptions)))
         f.writelines('{} {}\n'.format(word, ' '.join(transcriptions)))
 
 #with codecs.open('new_simple_words.json', mode='w', encoding='utf-8', errors='ignore') as fp:
