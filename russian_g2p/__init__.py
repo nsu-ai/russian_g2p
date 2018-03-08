9f896ec67771e583e1315454547a5919561cefa3
project <masha-1998-b@mail.ru>
2018-03-08 22:56:01 +0700

    ver.1.0.1

--- russian_g2p/__init__.py
+++ russian_g2p/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['Grapheme2Phoneme', 'Accentor']
+__all__ = ['Grapheme2Phoneme', 'Accentor', 'RulesForGraphemes', 'Preprocessor', 'Transcription']
 
+from russian_g2p.RulesForGraphemes import *
 from russian_g2p.Grapheme2Phoneme import *
 from russian_g2p.Accentor import *
