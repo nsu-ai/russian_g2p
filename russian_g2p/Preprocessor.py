5a93895347002e521ddf9ef0d6897d67c527e2d0
project <masha-1998-b@mail.ru>
2018-03-17 16:51:27 +0700

    with rnnmorph

--- russian_g2p/Preprocessor.py
+++ russian_g2p/Preprocessor.py
@@ -1,30 +1,26 @@
 from re import sub
 
-import treetaggerwrapper
+from rnnmorph.predictor import RNNMorphPredictor
 
 
 class Preprocessor():
 
     def __init__(self):
-        self.__tagger = treetaggerwrapper.TreeTagger(TAGLANG='ru')
+        self.predictor = RNNMorphPredictor()
 
     def gettags(self, text):
-        analysis = treetaggerwrapper.make_tags(self.__tagger.tag_text(text))
-        words_and_tags= []
+        analysis = self.predictor.predict_sentence_tags(text)
+        words_and_tags = []
         for word in analysis:
             word_and_tag = []
-            try:
-                word_and_tag.append(word.word)
-                word_and_tag.append(word.pos)
-            except AttributeError:
-                word_and_tag.append(word.what)
-                word_and_tag.append('uknown')
+            word_and_tag.append(word.word)
+            word_and_tag.append(word.pos + ' ' + word.tag)
             words_and_tags.append(word_and_tag)
         return words_and_tags
 
     def preprocessing(self, text):
-        text = sub('[\.\,\?\!\(\);:]+', '<sil>', text)
+        text = sub('[\.\,\?\!\(\);:]+', ' <sil>', text)
         text = sub(' [–-] ', '<sil>', text)
         text = sub('[\\\/@#\$%\^\&\*–_=+\'\"\|«»–-]+', '', text)
-        words_and_tags = self.gettags(text)
+        words_and_tags = self.gettags(text.split(' '))
         return words_and_tags
