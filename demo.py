ed6ad92042f4de6d0693ee288f7c90834d9f83e5
Ivan Bondarenko <bond005@yandex.ru>
2018-06-21 22:11:53 +0700

    Verbose mode has been added into the Transcription.

--- demo.py
+++ demo.py
@@ -26,69 +26,53 @@ def main():
     if len(dst_dir) > 0:
         assert os.path.isdir(dst_dir), 'Directory "{0}" does not exist!'.format(dst_dir)
 
-    src_fp = None
-    dst_fp = None
-    counter = 0
-    transcriptor = Transcription(exception_for_unknown=True)
-    silence = '<sil>'
     re_for_russian_letters = re.compile(r'[^АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя]+')
-    try:
-        src_fp = codecs.open(src_name, mode='r', encoding='utf-8', errors='ignore')
-        dst_fp = codecs.open(dst_name, mode='w', encoding='utf-8', errors='ignore')
+    source_lines_v1 = []
+    source_lines_v2 = []
+    with codecs.open(src_name, mode='r', encoding='utf-8', errors='ignore') as src_fp:
         cur_line = src_fp.readline()
         while len(cur_line) > 0:
-            prep_line = cur_line.strip()
-            if len(prep_line) > 0:
-                try:
-                    pronunciation = transcriptor.transcribe(prep_line)
-                except:
-                    pronunciation = None
-                if pronunciation is not None:
-                    if len(pronunciation) > 0:
-                        transcription_v1 = [silence]
-                        for cur_part in pronunciation:
-                            transcription_v1 += cur_part
-                            transcription_v1.append(silence)
-                    else:
-                        transcription_v1 = None
-                else:
-                    transcription_v1 = None
-                source_text = ' '.join(filter(lambda it: len(it) > 0, re_for_russian_letters.split(prep_line)))
-                try:
-                    pronunciation = transcriptor.transcribe(source_text)
-                except:
-                    pronunciation = None
-                if pronunciation is not None:
-                    if len(pronunciation) > 0:
-                        transcription_v2 = [silence]
-                        for cur_part in pronunciation:
-                            transcription_v2 += cur_part
-                            transcription_v2.append(silence)
-                    else:
-                        transcription_v2 = None
-                else:
-                    transcription_v2 = None
-                if (transcription_v1 is not None) and (transcription_v2 is not None):
-                    if args.pair_order == 'text-pronunciation':
-                        dst_fp.write('{0}\t{1}\n'.format(source_text.lower(), ' '.join(transcription_v1)))
-                    else:
-                        dst_fp.write('{0}\t{1}\n'.format(' '.join(transcription_v1), source_text.lower()))
-                    if transcription_v2 != transcription_v1:
-                        if args.pair_order == 'text-pronunciation':
-                            dst_fp.write('{0}\t{1}\n'.format(source_text.lower(), ' '.join(transcription_v2)))
-                        else:
-                            dst_fp.write('{0}\t{1}\n'.format(' '.join(transcription_v2), source_text.lower()))
+            prep_line_v1 = cur_line.strip()
+            if len(prep_line_v1) > 0:
+                source_lines_v1.append(prep_line_v1)
+                prep_line_v2 = ' '.join(filter(lambda it: len(it) > 0, re_for_russian_letters.split(prep_line_v1)))
+                source_lines_v2.append(prep_line_v2.strip())
             cur_line = src_fp.readline()
-            counter += 1
-            if (counter % 1000) == 0:
-                print('{0} texts...'.format(counter))
-        if (counter % 1000) != 0:
-            print('{0} texts...'.format(counter))
-    finally:
-        if src_fp is not None:
-            src_fp.close()
-        if dst_fp is not None:
-            dst_fp.close()
+
+    transcriptor = Transcription(exception_for_unknown=True, verbose=True)
+    pronunciations_v1 = transcriptor.transcribe(source_lines_v1)
+    pronunciations_v2 = transcriptor.transcribe(source_lines_v2)
+    del transcriptor
+    silence = '<sil>'
+    with codecs.open(dst_name, mode='w', encoding='utf-8', errors='ignore') as dst_fp:
+        for line_idx in range(len(source_lines_v1)):
+            pronunciation_v1 = pronunciations_v1[line_idx]
+            if len(pronunciation_v1) > 0:
+                transcription_v1 = [silence]
+                for cur_part in pronunciation_v1:
+                    transcription_v1 += cur_part
+                    transcription_v1.append(silence)
+            else:
+                transcription_v1 = []
+            pronunciation_v2 = pronunciations_v2[line_idx]
+            if len(pronunciation_v2) > 0:
+                transcription_v2 = [silence]
+                for cur_part in pronunciation_v2:
+                    transcription_v2 += cur_part
+                    transcription_v2.append(silence)
+            else:
+                transcription_v2 = []
+            source_text = source_lines_v2[line_idx]
+            if (len(transcription_v1) > 0) and (len(transcription_v2) > 0):
+                if args.pair_order == 'text-pronunciation':
+                    dst_fp.write('{0}\t{1}\n'.format(source_text.lower(), ' '.join(transcription_v1)))
+                else:
+                    dst_fp.write('{0}\t{1}\n'.format(' '.join(transcription_v1), source_text.lower()))
+                if transcription_v2 != transcription_v1:
+                    if args.pair_order == 'text-pronunciation':
+                        dst_fp.write('{0}\t{1}\n'.format(source_text.lower(), ' '.join(transcription_v2)))
+                    else:
+                        dst_fp.write('{0}\t{1}\n'.format(' '.join(transcription_v2), source_text.lower()))
 
 
 if __name__ == '__main__':
--- russian_g2p/Transcription.py
+++ russian_g2p/Transcription.py
@@ -4,23 +4,46 @@ from russian_g2p.Grapheme2Phoneme import Grapheme2Phoneme
 
 
 class Transcription:
-    def __init__(self, exception_for_unknown: bool=False, batch_size: int=64):
+    def __init__(self, exception_for_unknown: bool=False, batch_size: int=64, verbose=False):
         self.__preprocessor = Preprocessor(batch_size=batch_size)
         self.__accentor = Accentor(exception_for_unknown=exception_for_unknown)
         self.__g2p = Grapheme2Phoneme()
+        self.verbose = verbose
 
     def transcribe(self, texts: list):
         all_words_and_tags = self.__preprocessor.preprocessing(texts)
+        if self.verbose:
+            print('All texts have been preprocessed...')
+        n_texts = len(texts)
+        n_data_parts = 100
+        part_size = n_texts // n_data_parts
+        while (part_size * n_data_parts) < n_texts:
+            part_size += 1
+        data_counter = 0
+        part_counter = 0
         total_result = []
         for cur_words_and_tags in all_words_and_tags:
-            accented_text = self.__accentor.do_accents(cur_words_and_tags)
-            tmp = ' '.join(accented_text[0])
-            tmp = ' ' + tmp
-            phonetic_words = tmp.split(' <sil>')
-            result = []
-            for phonetic_word in phonetic_words:
-                if len(phonetic_word) != 0:
-                    phonemes = self.__g2p.phrase_to_phonemes(phonetic_word)
-                    result.append(phonemes)
+            try:
+                accented_text = self.__accentor.do_accents(cur_words_and_tags)
+            except:
+                accented_text = []
+            if len(accented_text) > 0:
+                tmp = ' '.join(accented_text[0])
+                tmp = ' ' + tmp
+                phonetic_words = tmp.split(' <sil>')
+                result = []
+                for phonetic_word in phonetic_words:
+                    if len(phonetic_word) != 0:
+                        phonemes = self.__g2p.phrase_to_phonemes(phonetic_word)
+                        result.append(phonemes)
+            else:
+                result = []
             total_result.append(result)
+            data_counter += 1
+            if (part_size > 0) and self.verbose:
+                if (data_counter % part_size) == 0:
+                    part_counter += 1
+                    print('{0}% of texts have been processed...'.format(part_counter))
+        if (part_counter < n_data_parts) and self.verbose:
+            print('100% of texts have been processed...')
         return total_result
